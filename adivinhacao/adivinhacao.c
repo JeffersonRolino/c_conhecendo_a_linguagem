f9df320b55ffd6d2cd5aae54841cd0b02e1b01c2
jeffersonrolino <jefferson.rolino@gmail.com>
2025-01-05 04:08:29 -0300

    Capítulo 01 - Aula 06: Lidando com erros

---
+++ adivinhacao/adivinhacao.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    // Imprime o cabeçalho do nosso jogo
+    printf("******************************************\n");
+    printf("* Bem vindo ao nosso jogo de adivinhacao *\n");
+    printf("******************************************\n");
+    return 0;
+}
--- adivinhacao/programa.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Bem vindo ao nosso jogo de adivinhacao");
-    return 0;
-}
