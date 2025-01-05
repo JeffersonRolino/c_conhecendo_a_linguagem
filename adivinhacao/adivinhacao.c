4e9eaf5d7e0e71792e1e58671b42dd8243d813f8
jeffersonrolino <jefferson.rolino@gmail.com>
2025-01-05 04:14:50 -0300

    Capítulo 01 - Aula 07: Guardando o número secreto

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -5,5 +5,10 @@ int main(){
     printf("******************************************\n");
     printf("* Bem vindo ao nosso jogo de adivinhacao *\n");
     printf("******************************************\n");
+
+    int numero_secreto = 42;
+
+    printf("O numero %d e o secreto. Nao conta para ninguem!\n", numero_secreto);
+
     return 0;
 }
