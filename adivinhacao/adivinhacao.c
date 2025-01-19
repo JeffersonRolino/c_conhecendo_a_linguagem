c7404d6e4ddd7991a92a4a3dcace931962a78a7a
jeffersonrolino <jefferson.rolino@gmail.com>
2025-01-19 20:21:20 -0300

    Capítulo 04 - Aula 01: Acrescentando níveis de dificuldade

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -15,11 +15,33 @@ int main(){
 
     int numero_secreto = numero_grande % 100;
     int chute;
-    int tentativa = 1;
     double pontos = 1000;
 
-    while(1){
-        printf("Tentativa %d\n", tentativa);
+    int nivel;
+    printf("Qual o nivel de dificuldade?\n");
+    printf("(1) Facil - (2) Medio - (3) Dificil\n\n");
+    printf("Escolha: ");
+    scanf("%d", &nivel);
+
+    int numero_de_tentativas;
+
+    switch (nivel) {
+        case 1:
+            numero_de_tentativas = 20;
+            break;
+        case 2:
+            numero_de_tentativas = 15;
+            break;
+        case 3:
+            numero_de_tentativas = 5;
+            break;
+        default:
+            numero_de_tentativas = 10;
+            break;
+    }
+
+    for(int i = 1; i <= numero_de_tentativas; i++){
+        printf("Tentativa %d\n", i);
         printf("Qual e o seu chute? ");
 
         scanf("%d", &chute);
@@ -35,6 +57,9 @@ int main(){
 
         if(acertou){
             printf("Parabens! Voce acertou!\n");
+            printf("Fim do jogo!\n");
+            printf("Voce acertou em %d tentativas!\n", i);
+            printf("Total de pontos: %.1f\n", pontos);
             break;
         }
         else if(maior){
@@ -44,16 +69,13 @@ int main(){
             printf("Seu chute foi menor que o numero secreto\n\n");
         }
 
-        tentativa++;
-
         double pontos_perdidos = abs(chute - numero_secreto) / (double) 2;
 
         pontos = pontos - pontos_perdidos;
     }
 
+    printf("Voce perdeu! Seu numero de tentativas acabou...\n");
     printf("Fim do jogo!\n");
-    printf("Voce acertou em %d tentativas!\n", tentativa);
-    printf("Total de pontos: %.1f\n", pontos);
 
     return 0;
 }
